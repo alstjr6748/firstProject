4c6b8dfecd512ab60b7a5a1755a87fdbe047ee18
admin <admin@DESKTOP-6AS9MCG>
2023-05-30 12:28:41 +0900

    1

---
+++ HelloApp/src/com/yedam/HelloApp.java
@@ -0,0 +1,11 @@
+package com.yedam;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// main 메소드 시작
+				System.out.println("Hello, World");
+				System.out.println("git 연동");
+	}
+
+}
---
+++ HelloApp/src/com/yedam/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yedam;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("git 연동");
+
+
+	}
+
+}
